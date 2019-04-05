9d493897887c0f275606885cf760baa07245e8a4
MASSYNAS HAMLAT <massynas.hamlat@atos.net>
2019-04-05 12:27:43 +0200

    arthur

---
+++ README.md
@@ -0,0 +1,24 @@
+# Formation Vue.js in depth
+
+Slides de présentation disponibles ici: https://github.com/SDCOAdvocates/Vue-training/raw/master/presenter_slides.pptx
+
+Support de formation disponible ici: https://sylvainpv.gitlab-pages.kazan.atosworldline.com/formation-vue-node/
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out the [Nuxt.js](https://github.com/nuxt/nuxt.js) and [Vuetify.js](https://vuetifyjs.com/) documentation.
---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app :dark="dark">
+    <v-navigation-drawer
+      :mini-variant.sync="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="dark = !dark"
+      >
+        <v-icon v-html="dark ? 'brightness_high' : 'brightness_low'"></v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      dark: false,
+      items: [
+        { icon: 'home', title: 'Accueil', to: '/' },
+        { icon: 'slideshow', title: 'Presentation', to: '/presentation' },
+        { icon: 'build', title: 'Outillage', to: '/tools' },
+        { icon: 'description', title: 'Vues', to: '/vues' },
+        { icon: 'code', title: 'Directives', to: '/directives' },
+        { icon: 'dashboard', title: 'Composants', to: '/components' },
+        { icon: 'restore_page', title: 'Réactivité', to: '/reactivity' },
+        { icon: 'store', title: "Gestion d'état", to: '/state' },
+        { icon: 'router', title: 'Routage', to: '/routing' },
+        { icon: 'http', title: 'Requêtes', to: '/http' },
+        { icon: 'loop', title: 'Réutilisabilité', to: '/reusability' },
+        { icon: 'extension', title: 'Ecosystème', to: '/ecosystem' }
+      ],
+      miniVariant: false,
+      title: 'Vue.js in depth'
+    }
+  }
+}
+</script>
